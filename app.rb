6762c4fcf1a4f94d84f86646603dd56973ba40d8
Graham Gatus <ggatus@atlassian.com>
2015-11-08 19:11:53 +1100

    fixed syntax of invalid message

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ class HangpersonApp < Sinatra::Base
         flash[:message] = "You have already used that letter."
       end
     rescue
-      flash[:message] = "invalid guess."
+      flash[:message] = "Invalid guess."
     end
       redirect '/show'
   end
